ed6ddd83f6278e89d893577dba0fc3e92ba0340c
DazorPlasma <dazorplasma@proton.me>
2023-11-01 21:41:50 +0200

    update

---
+++ src/fizzy.rs
@@ -0,0 +1,29 @@
+use std::borrow::Cow;
+
+const FIZZBUZZ_MAP: [(i32, &str); 3] = [(15, "FizzBuzz"), (3, "Fizz"), (5, "Buzz")];
+
+pub trait Fizzy {
+    /// Transforms the given input into its FizzBuzz counterpart
+    fn fizzy(&self) -> Cow<'static, str>;
+}
+
+impl Fizzy for i32 {
+    fn fizzy(&self) -> Cow<'static, str> {
+        for (divisor, mapped_text) in FIZZBUZZ_MAP {
+            if self % divisor == 0 {
+                return Cow::Borrowed(mapped_text);
+            }
+        }
+        Cow::Owned(self.to_string())
+    }
+}
+
+pub fn play_fizzbuzz(iterator: impl IntoIterator<Item = i32>) -> String {
+    let mut output = String::new();
+    for i in iterator {
+        output.push_str(&i.fizzy());
+        output.push('\n');
+    }
+    output.shrink_to_fit();
+    output
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-trait Fizzy {
-    fn fizzify(&self) -> String;
-}
-
-impl Fizzy for i32 {
-    fn fizzify(&self) -> String {
-        match (self % 3, self % 5) {
-            (0, 0) => String::from("FizzBuzz"),
-            (0, _) => String::from("Fizz"),
-            (_, 0) => String::from("Buzz"),
-            (_, _) => self.to_string()
-        }
-    }
-}
+mod fizzy;
 
 fn main() {
-    for i in 1..=100 {
-        println!("{}", i.fizzify());
-    }
+    println!("{}", fizzy::play_fizzbuzz(0..=100));
 }
